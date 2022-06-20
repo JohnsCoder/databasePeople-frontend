ad2763ec064ff29c9a634f73d3ca483ac87173d6
JohnsCoder <joaopdossantos1515@gmail.com>
2022-06-20 10:04:17 -0300

    adding icon

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import styles from "../styles/components/cards.module.css";
 import { EditDialogWindow } from "./dialog";
-// import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-// import { faPencil } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPencil } from "@fortawesome/free-solid-svg-icons";
 const Cards = (props) => {
   const [display, setDisplay] = useState("none");
   const showWindow = () => setDisplay("block");
@@ -19,12 +19,15 @@ const Cards = (props) => {
         email={props.email}
         salary={props.salary}
       />
-      <div className={styles.cards} onClick={showWindow}>
-        <span>
-          {props.first_name} {props.last_name}
-        </span>
-        <span>{props.email}</span>
-        <span>{props.salary}</span>
+      <div className={styles.container} onClick={showWindow}>
+        <div className={styles.cards}>
+          <span>
+            {props.first_name} {props.last_name}
+          </span>
+          <span>{props.email}</span>
+          <span>{props.salary}</span>
+        </div>
+        <FontAwesomeIcon icon={faPencil} className={styles.icon} />
       </div>
     </>
   );
